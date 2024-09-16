b9d0dbfc2c6b0dad0747e665b29d3d417c4b6694
Awab Saif <132479530+AwabSaif@users.noreply.github.com>
2024-09-16 19:31:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+var multer = require('multer');
+var path = require('path');
+
+var app = express();
+
+
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
+});
